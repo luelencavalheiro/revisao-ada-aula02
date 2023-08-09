711fc850beabf9be7107e48f8a2bd4ca7e31b708
Luelen Cavalheiro <luelencavalheiro@hotmail.com>
2023-08-09 18:01:27 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_02_revisao_ada
+{
+    internal class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return preco * quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            this.quantidade = this.quantidade + quantidade;
+
+        }
+         public void RemoverProdutos(int quantidade)
+        {
+            this.quantidade = this.quantidade - quantidade;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+using aula_02_revisao_ada;
+
+Console.WriteLine("Qual o nome do produto?");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Qual o preço?");
+double.TryParse(Console.ReadLine(), out double preco);
+
+Console.WriteLine("Qual a quantidade?");
+int.TryParse(Console.ReadLine(), out int quantidade);
+
+Produto produto = new Produto (nome, preco, quantidade);
+
+Console.WriteLine("Nome Produto:" + produto.nome);
+Console.WriteLine("Preço:" + produto.preco);
+Console.WriteLine("Quantidade:" + produto.quantidade);
+Console.WriteLine("Valor Total:" + produto.ValorTotalEmEstoque());
+
+Console.WriteLine("Qual quantidade você gostaria de adicionar?");
+int.TryParse(Console.ReadLine(), out int quantidadeAdicional);
+produto.AdicionarProdutos(quantidadeAdicional);
+
+Console.WriteLine("Nome Produto:" + produto.nome);
+Console.WriteLine("Preço:" + produto.preco);
+Console.WriteLine("Quantidade:" + produto.quantidade);
+Console.WriteLine("Valor Total:" + produto.ValorTotalEmEstoque());
+
+Console.WriteLine("Qual quantidade você gostaria de remover?");
+int.TryParse(Console.ReadLine(), out int quantidadeRemover);
+produto.RemoverProdutos(quantidadeRemover);
+
+Console.WriteLine("Nome Produto:" + produto.nome);
+Console.WriteLine("Preço:" + produto.preco);
+Console.WriteLine("Quantidade:" + produto.quantidade);
+Console.WriteLine("Valor Total:" + produto.ValorTotalEmEstoque());
+
+
+
+
+
+
+
+
+
+
+
+
+
